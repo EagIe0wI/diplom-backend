117fb8cedf5d0ed2eb4ae59f2ea8b279a0d5cd82
EagIe0wI <anya230704@gmail.com>
2026-06-10 07:47:58 +0300

    Hardcode SECRET_KEY for cloud deploy stability

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+^i3^hlho&0ng7100-ey17j=0@g+z9)(naxxa^u0&m&981pk@u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
